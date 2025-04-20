324950ffe37ccdb1c652717c8b6871626331706e
BellamyPhan <bellamyphan@icloud.com>
2025-04-20 01:03:02 -0500

    Initial commit money-app-api (Spring Boot)

---
+++ money-app-api/money-app-api/src/main/java/com/bellamyphan/moneyapp/money_app_api/MoneyAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.bellamyphan.moneyapp.money_app_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyAppApiApplication.class, args);
+	}
+
+}
