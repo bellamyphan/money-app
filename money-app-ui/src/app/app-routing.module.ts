b61f6a133683af70c8a7792163c335a24681809b
BellamyPhan <bellamyphan@icloud.com>
2025-04-20 23:45:35 -0500

    refactor: remove unused TransactionDetailsComponent route from app routing

--- money-app-ui/src/app/app-routing.module.ts
+++ money-app-ui/src/app/app-routing.module.ts
@@ -3,24 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { TransactionListComponent } from './transaction-list/transaction-list.component';
 import { TransactionCreateComponent } from './transaction-create/transaction-create.component';
 import { TransactionUpdateComponent } from './transaction-update/transaction-update.component';
-import { TransactionDetailsComponent } from './transaction-details/transaction-details.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/transactions', pathMatch: 'full' }, // Redirect to transactions if the path is empty
-  { path: 'transactions', component: TransactionListComponent }, // Route for listing transactions
-  { path: 'transactions/new', component: TransactionCreateComponent }, // New route for creating a transaction
-  {
-    path: 'transactions/:id/update',
-    component: TransactionUpdateComponent,
-    data: { renderMode: 'default' } // Prevent prerendering on dynamic route
-  },
-  {
-    path: 'transactions/:id/details',
-    component: TransactionDetailsComponent,
-    data: { renderMode: 'default' } // Prevent prerendering on dynamic route
-  },
+  {path: '', redirectTo: '/transactions', pathMatch: 'full'}, // Redirect to transactions if the path is empty
+  {path: 'transactions', component: TransactionListComponent}, // Route for listing transactions
+  {path: 'transactions/new', component: TransactionCreateComponent}, // New route for creating a transaction
+  {path: 'transactions/:id/update', component: TransactionUpdateComponent} // Route for editing a transaction
 ];
-
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
